ae7781953390188d9bf9df1aebd66bc9bff64ca3
Karim <karimsalah795@gmail.com>
2025-05-11 14:49:38 +0300

    add best seller text

--- lib/Features/Home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:booksy/Features/Home/presentation/views/widgets/book_list_view.dart';
 import 'package:booksy/Features/Home/presentation/views/widgets/custom_app_.dart';
- import 'package:flutter/material.dart';
+import 'package:booksy/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -8,9 +10,17 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [CustomAppBar(), BookListView()],
+      children: [
+        CustomAppBar(),
+        BookListView(),
+        SizedBox(
+          height: 14,
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: 250),
+          child: Text("Best Seller", style: Booksy.mediumText),
+        )
+      ],
     );
   }
 }
-
-
--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 abstract class Booksy {
   static const String slogan = 'Ready to read?';
   static Color kPrimaryColor = const Color(0xFF100B20);
-  static const  String logo = "assets/BOOKLY.png";
-   static const  String test = "assets/test_image.png";
+  static const String logo = "assets/BOOKLY.png";
+  static const String test = "assets/test_image.png";
+  static const mediumText = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
 }
