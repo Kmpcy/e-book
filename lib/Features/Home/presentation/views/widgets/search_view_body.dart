5b9ed5bab41196bc60117bd15db6f6343c20e9c0
Karim <karimsalah795@gmail.com>
2025-05-26 09:52:30 +0300

    create book mdels

--- lib/Features/Home/presentation/views/widgets/search_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/search_view_body.dart
@@ -1,23 +1,35 @@
-  import 'package:booksy/Features/Home/presentation/views/widgets/custom_text_field.dart';
+import 'package:booksy/Features/Home/presentation/views/widgets/custom_text_field.dart';
 import 'package:booksy/Features/Home/presentation/views/widgets/serach_result.dart';
 import 'package:flutter/material.dart';
- 
+import 'package:flutter/widgets.dart';
+
 class SerachViewBody extends StatelessWidget {
   const SerachViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Column(
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
         Padding(
           padding: EdgeInsets.all(8.0),
-          child:   CustomTextField(),
+          child: CustomTextField(),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            "   Search Result",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(
+          height: 10,
         ),
         SearchResult(),
       ],
     );
   }
 }
-
-
-
