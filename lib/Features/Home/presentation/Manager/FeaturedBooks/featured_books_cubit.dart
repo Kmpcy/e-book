baabc94b6154980805735d35cbe6e869ffc588e7
Karim <karimsalah795@gmail.com>
2025-05-28 10:00:11 +0300

    featured books cubit d1

---
+++ lib/Features/Home/presentation/Manager/FeaturedBooks/featured_books_cubit.dart
@@ -0,0 +1,39 @@
+import 'package:bloc/bloc.dart';
+import 'package:booksy/Features/Home/Data/models/book_model/book_model.dart';
+import 'package:booksy/Features/Home/Repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+
+final HomeRepo homeRepo;
+
+Future <void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeatureBooks();
+    result.fold(
+      (failure) => emit(FeaturedBooksFailed(failure.errorMessage)),
+      (books) => emit(FeaturedBooksSuccess(books)),
+    );
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
